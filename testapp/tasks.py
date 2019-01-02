b2b8d9b8d89aad93c3a14fd0f5658517d52acaa2
minoh0201 <minoh@vt.edu>
2019-01-02 14:41:33 -0500

    prodigal

--- testapp/tasks.py
+++ testapp/tasks.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 
 from celery import shared_task
 
-
 @shared_task
 def test(param):
     return 'The test task executed with argument "%s" ' % param
@@ -11,9 +10,6 @@ def test(param):
 def test2(param):
     return 'test2: "%s"' % param
 
-@shared_task
-def test3(param):
-    return 'test3: "%s"' % param
 
 import subprocess
 import webapp.settings as SETTING
