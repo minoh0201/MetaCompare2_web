e3350474cee1f069b1cf8e5c2b3e91f07e077c1c
minoh0201 <minoh@vt.edu>
2019-01-06 21:36:50 -0500

    metacmp2 process

--- testapp/tasks.py
+++ testapp/tasks.py
@@ -30,10 +30,10 @@ def runSample(sample_file):
 
     subprocess.call(["prodigal", "-i", filepath_contig, "-d", filepath_prod, "-p", "meta", "-o", filepath_prod_log])
 
-    subprocess.call(["which", "python"])
-    subprocess.call(["python", "--version"])
+    subprocess.call(["which", "python3"])
+    subprocess.call(["python3", "--version"])
 
-    subprocess.call(["python", "/home/minoh/MetaCompare2_cmd/metacmp2.py", "-c", filepath_contig, "-g", filepath_prod, "-o", sample_dir_path])
+    subprocess.call(["python3", "/home/minoh/MetaCompare2_cmd/metacmp2.py", "-c", filepath_contig, "-g", filepath_prod, "-o", sample_dir_path])
 
     return 'done'
 
