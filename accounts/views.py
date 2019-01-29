47ca0e711a477e6f43456d9b811c5bec38847e10
minoh0201 <minoh@vt.edu>
2019-01-28 21:14:17 -0500

    asking e-mail for registration

--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,15 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 
+from django.contrib.auth.models import User
+
+class UserCreationForm_email(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "email")
+
+
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm_email
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
